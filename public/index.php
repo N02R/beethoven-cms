40df104529b3f19fe12669f125f35226b61b6e32
N02R <nh9338743@gmail.com>
2026-07-04 16:17:17 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -5,10 +5,15 @@ require_once __DIR__ . '/../core/Request.php';
 require_once __DIR__ . '/../core/Router.php';
 
 require_once __DIR__ . '/../core/Database.php';
+require_once __DIR__ . '/../core/bootstrap.php';
+require_once __DIR__ . '/../core/Database.php';
+
+require_once __DIR__ . '/../app/Models/Page.php';
+require_once __DIR__ . '/../app/Models/Section.php';
+require_once __DIR__ . '/../app/Models/Block.php';
 
 require_once __DIR__ . '/../app/Controllers/Controller.php';
 require_once __DIR__ . '/../app/Controllers/HomeController.php';
-require_once __DIR__ . '/../app/Models/Page.php';
 
 $request = new Request();
 $router = new Router();
