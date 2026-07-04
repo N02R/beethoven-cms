b7517ea44703946a15e3a9fa124b4fdc56bad783
N02R <nh9338743@gmail.com>
2026-07-04 15:20:13 +0300

    update

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,6 +1,35 @@
 <?php
 
-require_once __DIR__ . '/Database.php';
+/*
+|--------------------------------------------------------------------------
+| Load Application Configuration
+|--------------------------------------------------------------------------
+*/
 
-$db = Database::getInstance();
-$pdo = $db->getConnection();
+$config = require __DIR__ . '/../config/app.php';
+
+
+/*
+|--------------------------------------------------------------------------
+| Timezone
+|--------------------------------------------------------------------------
+*/
+
+date_default_timezone_set($config['timezone']);
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Constants
+|--------------------------------------------------------------------------
+*/
+
+define('APP_NAME', $config['name']);
+
+define('APP_URL', rtrim($config['url'], '/'));
+
+define('APP_ENV', $config['environment']);
+
+define('APP_DEBUG', $config['debug']);
+
+define('APP_LOCALE', $config['locale']);
