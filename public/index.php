6b29f671fafa87be7215014cb31d9ea39bfcc6d3
N02R <nh9338743@gmail.com>
2026-07-04 15:52:24 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,30 @@
 <?php
 
+require_once __DIR__ . '/../core/bootstrap.php';
+require_once __DIR__ . '/../core/Request.php';
+require_once __DIR__ . '/../core/Router.php';
+
 require_once __DIR__ . '/../core/Database.php';
 
-$db = Database::getInstance()->connection();
+require_once __DIR__ . '/../app/Controllers/Controller.php';
+require_once __DIR__ . '/../app/Controllers/HomeController.php';
+require_once __DIR__ . '/../app/Models/Page.php';
 
-echo "DB Connected Successfully 🚀";
+$request = new Request();
+$router = new Router();
+
+/*
+|---------------------------
+| ROUTES
+|---------------------------
+*/
+
+$router->get('/', 'HomeController@index');
+
+/*
+|---------------------------
+| RUN APP
+|---------------------------
+*/
+
+$router->resolve($request);
