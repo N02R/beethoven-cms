5f8b76cba9684dcfc3c9e9cd7e5d09792a399bb2
N02R <nh9338743@gmail.com>
2026-07-04 16:47:29 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,24 +1,47 @@
 <?php
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once __DIR__ . '/../core/bootstrap.php';
+
+/*
+|---------------------------
+| CORE
+|---------------------------
+*/
+
 require_once __DIR__ . '/../core/Request.php';
 require_once __DIR__ . '/../core/Router.php';
-
 require_once __DIR__ . '/../core/Database.php';
 require_once __DIR__ . '/../core/bootstrap.php';
-require_once __DIR__ . '/../core/Database.php';
+
+/*
+|---------------------------
+| MODELS
+|---------------------------
+*/
 
 require_once __DIR__ . '/../app/Models/Page.php';
 require_once __DIR__ . '/../app/Models/Section.php';
 require_once __DIR__ . '/../app/Models/Block.php';
 
+/*
+|---------------------------
+| CONTROLLERS
+|---------------------------
+*/
+
 require_once __DIR__ . '/../app/Controllers/Controller.php';
 require_once __DIR__ . '/../app/Controllers/HomeController.php';
 
+/*
+|---------------------------
+| INIT APP
+|---------------------------
+*/
+
 $request = new Request();
-$router = new Router();
+$router  = new Router();
 
 /*
 |---------------------------
@@ -30,7 +53,7 @@ $router->get('/', 'HomeController@index');
 
 /*
 |---------------------------
-| RUN APP
+| RUN
 |---------------------------
 */
 
