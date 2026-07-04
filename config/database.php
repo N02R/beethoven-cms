30488f18de92587b4964e7123d57ca091ffaac7f
N02R <nh9338743@gmail.com>
2026-07-04 15:44:37 +0300

    update database.php

--- config/database.php
+++ config/database.php
@@ -1,40 +1,11 @@
 <?php
 
-class Database
-{
-    private static ?Database $instance = null;
-    private PDO $connection;
+return [
 
-    private function __construct()
-    {
-        $config = require __DIR__ . '/../config/database.php';
+    'host' => '127.0.0.1',
+    'dbname' => 'cms_dev',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
 
-        try {
-            $this->connection = new PDO(
-                "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}",
-                $config['username'],
-                $config['password']
-            );
-
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-        } catch (PDOException $e) {
-            die("Database connection failed: " . $e->getMessage());
-        }
-    }
-
-    public static function getInstance(): Database
-    {
-        if (self::$instance === null) {
-            self::$instance = new Database();
-        }
-
-        return self::$instance;
-    }
-
-    public function connection(): PDO
-    {
-        return $this->connection;
-    }
-}
+];
